db131230033bac99749a10740670e694283995bf
mariac <mariaclararodriguesdasilaviei@gmail.com>
2025-05-07 09:52:18 -0300

    consumo simples de uma API

--- src/pages/Detalhes/index.jsx
+++ src/pages/Detalhes/index.jsx
@@ -1,7 +1,28 @@
-export default function Detalhes() {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+export default function Home() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(( ) => {
+      axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10")
+           .then((res) => setPosts(res.data))
+    }, [])
+
+    console.log(posts)
     return (
     <>
-    <h1 className="text-2x1 font-bold">DETALHES</h1>
+    <div>
+        <h2>Lista de Posts</h2>
+        <ul>
+          {posts.map((piercetheveil) => (
+            <li key={piercetheveil.id}> 
+            <Link to ={`/detalhes/${piercetheveil.id}`}>{posts.title}</Link>
+
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
     );
     }
